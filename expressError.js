e4f048590bfe400697ce12b5b74f3fbfe66bf479
Bryce <bryce.cc.dunn@gmail.com>
2021-11-15 21:08:08 -0600

    Jest configuration and expressError

---
+++ expressError.js
@@ -0,0 +1,34 @@
+/** ExpressError extends the normal JS error so we can add additioinal information when instantiating an error 
+ * 
+ * Additional specific errors then extend ExpressError
+*/
+
+class ExpressError extends Error {
+    constructor(message, status) {
+        super();
+        this.message = message;
+        this.status = status;
+    }
+}
+
+// 404 NOT FOUND
+
+class NotFoundError extends ExpressError {
+    constructor(message = 'Not Found') {
+        super(message, 404);
+    }
+}
+
+// 401 UNAUTHORIZED
+
+class UnauthorizedError extends ExpressError {
+    constructor(message = 'Unauthorized') {
+        super(message, 401)
+    }
+}
+
+export default ExpressError
+export {
+    NotFoundError,
+    UnauthorizedError
+}
---
+++ jestSetup.js
@@ -0,0 +1,6 @@
+process.env.SECRET_KEY = 'asecret';
+process.env.PORT = '1234';
+process.env.NODE_ENV = 'envtest';
+process.env.DB_USER = 'user'
+process.env.DB_PASSWORD = 'password';
+process.env.DB_PORT = '5678';
