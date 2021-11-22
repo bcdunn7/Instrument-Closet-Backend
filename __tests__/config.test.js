6582eed8dcca22a6029e7586814567023d21233a
Bryce <bryce.cc.dunn@gmail.com>
2021-11-22 13:11:29 -0600

    Restructure Jest testing regarding enviornment variables

--- __tests__/config.test.js
+++ __tests__/config.test.js
@@ -1,10 +1,19 @@
 'use strict';
 
-import { SECRET_KEY, PORT, getDatabaseUri } from '../config';
+beforeAll(() => {
+    process.env.SECRET_KEY = 'asecret';
+    process.env.PORT = '1234';
+    process.env.NODE_ENV = 'envtest';
+    process.env.DB_USER = 'user'
+    process.env.DB_PASSWORD = 'password';
+    process.env.DB_PORT = '5678';
+})
 
 describe('config gathers variables from env', () => {
+
     it('works with static variables', () => {
 
+        const { SECRET_KEY, PORT } = require('../config');
         expect(SECRET_KEY).toEqual('asecret');
         expect(PORT).toEqual(1234);
         expect(process.env.NODE_ENV).toEqual('envtest');
@@ -15,6 +24,7 @@ describe('config gathers variables from env', () => {
     });
 
     it('correctly runs getDatabaseUrl', () => {
+        const { getDatabaseUri } = require('../config');
         expect(getDatabaseUri()).toEqual('postgresql://user:password@localhost:5678/instrument_closet')
 
         process.env.NODE_ENV = 'test';
@@ -26,6 +36,3 @@ describe('config gathers variables from env', () => {
         delete process.env.DB_PORT
     });
 });
-
-
-
